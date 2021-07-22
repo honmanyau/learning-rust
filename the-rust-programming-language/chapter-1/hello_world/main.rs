8abcfdc775508575353e50e400f8d579c1ff94ae
honmanyau <honmanyau@users.noreply.github.com>
2021-07-22 23:49:42 +1000

    Formatted.

--- the-rust-programming-language/chapter-1/hello_world/main.rs
+++ the-rust-programming-language/chapter-1/hello_world/main.rs
@@ -1,6 +1,6 @@
 fn main() {
-  println!("Hello, world!");
-  println!("G'day!");
-  println!("Moin moin!");
-  println!("はいさい！");
+    println!("Hello, world!");
+    println!("G'day!");
+    println!("Moin moin!");
+    println!("はいさい！");
 }
