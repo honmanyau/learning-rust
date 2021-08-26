2a3d593c257c876746e671a8785327c849f641d9
honmanyau <honmanyau@users.noreply.github.com>
2021-08-26 21:51:30 +1000

    Formatted.

--- the-rust-programming-language/chapter-12/minigrep/src/main.rs
+++ the-rust-programming-language/chapter-12/minigrep/src/main.rs
@@ -6,11 +6,10 @@ use minigrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args)
-        .unwrap_or_else(|err| {
-            println!("Problem parsing arguments: {}", err);
-            process::exit(1);
-        });
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("Query: {}", config.query);
     println!("Filename: {}", config.filename);
